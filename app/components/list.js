e9623ac8812bde47fa9d8c5c7b0c10c8be32686b
Karuna2505 <karunamathur14@gmail.com>
2024-01-17 23:25:02 +0530

    link_ui_update

--- app/components/list.js
+++ app/components/list.js
@@ -8,13 +8,6 @@ function list({ newsData, likedArticles, handleLiked }) {
         (article, index) =>
           article.description &&
           article.image_url && (
-            <Link
-              href={{
-                pathname: "/NewsDetail",
-                query: { ...article },
-              }}
-              key={article.title}
-            >
               <div className="max-w-max mx-12 my-6 rounded-2xl shadow-md shadow-white overflow-hidden md:max-w-6xl border-white border-2">
                 <div className="md:flex">
                   <div className="md:shrink-0">
@@ -24,7 +17,7 @@ function list({ newsData, likedArticles, handleLiked }) {
                       alt={article.title}
                     />
                   </div>
-                  <div className="p-8">
+                  <div className="p-8 flex flex-col">
                   <div className="flex justify-between">
                     <div className="block mt-1 text-lg leading-tight font-medium">
                       {article.title}
@@ -51,10 +44,20 @@ function list({ newsData, likedArticles, handleLiked }) {
                   </div>
                   </div>
                     <p className="mt-2">{article.description}</p>
+                    <div className="py-6 text-blue-200">
+                  <Link
+              href={{
+                pathname: "/NewsDetail",
+                query: { ...article },
+              }}
+              key={article.title}
+            >Read More</Link>
                   </div>
+                  </div>
+                 
                 </div>
+                
               </div>
-            </Link>
           )
       )}
     </div>
